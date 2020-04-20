8934a135d7cad676c4603577df2625441432e0fc
rumeshmohan <rumeshmohan5@gmail.com>
2020-04-20 21:22:42 +0530

    Validate Login and Register
    validations are done in frontend

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,36 +2,70 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {login} from '../../store/actions/authAction'
 import {Redirect} from 'react-router-dom'
+import * as validator from './Validations.js'
 
 class Login extends Component {
     state = {
-        email : '',
-        password: ''
+        email: '',
+        password: '',
+
+        errors: {
+            email: '',
+            password: ''
+        }
     }
-    handelChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
+
+    validateInputAndSetState = (id, value) => {
+        const errors = validator.validate(id, value, this.state.errors);
+        this.setState({errors, [id]: value});
+    }
+
+    handleChange = (e) => {
+        const { id, value } = e.target;
+        this.validateInputAndSetState(id, value);
+
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.login(this.state);
+
+        // iterate through the component state as key value pairs and
+        //  run the validation on each value.
+        // if the validation function handles that key value pair
+        //  then it is validated otherwise skipped
+        for (let [id, value] of Object.entries(this.state)) {
+            console.log(`${id} : ${value}`);
+            this.validateInputAndSetState(id, value);
+        }
+
+        // if error object is empty then the form is valid
+        const isFormValid = validator.isErrorObjectEmpty(this.state.errors);
+        // submit if the form is valid  
+        
+        if (isFormValid) {
+            console.log('Form Valid');
+            this.props.login(this.state);
+        } else {
+            console.log('Form invalid');
+        }
     }
+
     render() {
         const {authError, auth} = this.props;
         if(auth.uid) return <Redirect to='/'/>
         return (
             <div className="container">
-                <form onSubmit = {this.handleSubmit} className="white">
+                <form onSubmit = {this.handleSubmit} className="white" noValidate>
                     <h5 className = "grey-text text-darken-3">Login</h5>
                     <div className = "input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id= "email" onChange ={this.handelChange}/>
+                        <input type="email" id= "email" onChange ={this.handleChange} noValidate/>
+                        <div>{this.state.errors.email}</div>
                     </div>
                     <div className = "input-field">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handelChange}/>
+                        <input type="password" id="password" onChange={this.handleChange}/>
+                        <div>{this.state.errors.password}</div>
                     </div>
                     <div className="input-filed">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,49 +2,86 @@ import React, { Component } from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {register} from '../../store/actions/authAction'
+import * as validator from './Validations.js'
 
 class Register extends Component {
     
     state = {
-        eM : '',
+        em : '',
         pwd: '',
         fN:'',
         lN:'',
-        pNo:null
+        pNo: null,
+        errors: {
+            fN: '',
+            lN: '',
+            eM: '',
+            pwd: '',
+        },
     }
-    handelChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
+
+    validateInputAndSetState = (id, value) => {
+        const errors = validator.validate(id, value, this.state.errors);
+        this.setState({errors, [id]: value});
+    }
+
+    handleChange = (e) => {
+        const { id, value } = e.target;
+        this.validateInputAndSetState(id, value);
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.register(this.state);
-        this.props.history.push(this.state,'/create',); // After the registration is completed, create page will be loaded.
+
+        // iterate through the component state as key value pairs and
+        //  run the validation on each value.
+        // if the validation function handles that key value pair
+        //  then it is validated otherwise skipped
+        for (let [id, value] of Object.entries(this.state)) {
+            console.log(`${id} : ${value}`);
+            this.validateInputAndSetState(id, value);
+        }
+        
+        // if error object is empty then the form is valid
+        const isFormValid = validator.isErrorObjectEmpty(this.state.errors);
+        // submit if the form is valid     
+        if (isFormValid) {
+            console.log('Form is valid');
+            this.props.register(this.state);
+            this.props.history.push(this.state,'/create',); // After the registration is completed, create page will be loaded.
+        } else {
+            console.log('Form is invalid. Are errors displayed?');
+        }
+        
+        console.log(this.state);
     }
+    
     render() {
         const {auth,authError} = this.props;
         if(auth.uid) return <Redirect to='/create' />
         return (
             <div className="container">
-                <form onSubmit = {this.handleSubmit} className="white">
+                <form onSubmit = {this.handleSubmit} className="white" noValidate>
                     <h5 className = "grey-text text-darken-3">Register</h5>
                     <div className = "input-field">
                         <label htmlFor="email">First Name</label>
-                        <input type="text" id= "fN" onChange ={this.handelChange}/>
+                        <input type="text" id= "fN" onChange ={this.handleChange}/>
+                        <div>{this.state.errors.fN}</div>
                     </div>
                     <div className = "input-field">
                         <label htmlFor="email">Last Name</label>
-                        <input type="text" id= "lN" onChange ={this.handelChange}/>
+                        <input type="text" id= "lN" onChange ={this.handleChange}/>
+                        <div>{this.state.errors.lN}</div>
                     </div>
                     <div className = "input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id= "em" onChange ={this.handelChange}/>
+                        <input type="email" id= "em" onChange ={this.handleChange} noValidate/>
+                        <div>{this.state.errors.eM}</div>
                     </div>
                     <div className = "input-field">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="pwd" onChange={this.handelChange}/>
+                        <input type="password" id="pwd" onChange={this.handleChange}/>
+                        <div>{this.state.errors.pwd}</div>
                     </div>
                     <div className="input-filed">
                         <button className="btn pink lighten-1 z-depth-0" >Register</button>
